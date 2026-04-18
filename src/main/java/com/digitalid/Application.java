9ae16749f072afced4659128586abe74e53b7557
Caden Cheong <ylcheong@amazon.co.uk>
2026-04-18 16:45:40 +0100

    feat: architecture skeleton and directory structure + gitignore

---
+++ src/main/java/com/digitalid/Application.java
@@ -0,0 +1,10 @@
+package com.digitalid;
+
+/**
+ * Main application entry point.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Digital ID Management System");
+    }
+}
