10e3a700d0029ade8e29caa82541968509cc2ed1
Caden Cheong <ylcheong@amazon.co.uk>
2026-05-01 13:11:32 +0100

    feat create ChangeStatusUseCase

---
+++ src/main/java/com/digitalid/application/usecase/ChangeStatusUseCase.java
@@ -0,0 +1,47 @@
+package com.digitalid.application.usecase;
+
+import com.digitalid.application.port.in.UseCase;
+import com.digitalid.domain.model.OrganisationContext;
+import com.digitalid.domain.model.Worker;
+import com.digitalid.domain.model.WorkerStatus;
+import com.digitalid.domain.service.WorkerValidationService;
+import com.digitalid.application.port.out.WorkerRepository;
+import com.digitalid.application.request.ChangeStatusRequest;
+import com.digitalid.application.service.AuditService;
+
+
+public class ChangeStatusUseCase implements UseCase<ChangeStatusRequest, Worker> {
+
+    private final OrganisationContext org;
+    private final WorkerValidationService validator;
+    private final WorkerRepository repository;
+    private final AuditService logger;
+
+    public ChangeStatusUseCase (OrganisationContext org, WorkerValidationService validator, WorkerRepository repository, AuditService logger) {
+        this.org = org;
+        this.validator = validator;
+        this.repository = repository;
+        this.logger = logger;
+    }
+
+    public Worker execute(ChangeStatusRequest request) {
+
+        String reqWorkerId = request.getWorkerId();
+        WorkerStatus reqNewStatus = request.getNewStatus();
+
+        Worker worker = repository.findById(reqWorkerId);
+        validator.validateStatusChange(worker, reqNewStatus);
+
+        worker.changeStatus(reqNewStatus);
+
+        repository.save(worker);
+
+        // Logging
+        logger.log("CHANGE_STATUS", reqWorkerId, "Worker", org);
+
+        return worker;
+
+    }
+
+}
+
